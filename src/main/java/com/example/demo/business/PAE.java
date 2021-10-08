5df7b2ae75f3b7c7531a39176941bf723fd658f5
Bryan Grégoire <53735@etu.he2b.be>
2021-10-09 00:48:31 +0200

    refactor, use service instead Bean

--- src/main/java/com/example/demo/business/PAE.java
+++ src/main/java/com/example/demo/business/PAE.java
@@ -2,23 +2,21 @@ package com.example.demo.business;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import com.example.demo.model.Course;
+
+import org.springframework.stereotype.Service;
 
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-/*@Bean*/
+@Service
 public class PAE {
 
-    public ArrayList<Course> getCourses() {
-        Course cours1 = new Course("INT", "Introduction", 10);
-        Course cours2 = new Course("MAT1", "Mathematics 2", 3);
-        Course cours3 = new Course("CAI1", "Anglais", 2);
-        Course cours4 = new Course("DEV1", "Developpement 1", 10);
-        Course cours5 = new Course("DEV2", "Developpement 2", 10);
-        Course cours6 = new Course("WEBG2", "Developpement web 1", 5);
-        ArrayList<Course> cours = new ArrayList<>(Arrays.asList(cours1, cours2, cours3, cours4, cours5, cours6));
-        return cours;
+    public List<Course> getCourses() {
+        return Arrays.asList(new Course("INT", "Introduction", 10), new Course("MAT1", "Mathematics 2", 3),
+                new Course("CAI1", "Anglais", 2), new Course("DEV1", "Developpement 1", 10),
+                new Course("DEV2", "Developpement 2", 10), new Course("WEBG2", "Developpement web 1", 5));
     }
 }
